f36c8cd9aeaef930437b9360a1f7b1e522cb6912
1nt6ayrx <82008394+1nt6ayrx@users.noreply.github.com>
2021-04-05 20:19:10 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ $ErrorActionPreference = 'Stop'
 # dotnet build isn't used because it can't build net35 tfms
 #
 
+dotnet clean && dotnet nuget locals all --clear
 msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 Remove-Item Release\net35\*.pdb, Release\net35\*.xml, Release\net35\Test.Rename.*
