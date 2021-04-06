184f7a7aa0dcc10923905a941903da5602212082
1nt6ayrx <82008394+1nt6ayrx@users.noreply.github.com>
2021-04-05 20:59:56 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -5,13 +5,13 @@ $ErrorActionPreference = 'Stop'
 #
 
 dotnet clean && dotnet nuget locals all --clear
-dotnet restore
+dotnet restore de4dot.netframework.sln
 msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net35 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 Remove-Item Release\net35\*.pdb, Release\net35\*.xml, Release\net35\Test.Rename.*
 
 dotnet clean && dotnet nuget locals all --clear
-dotnet restore
+dotnet restore de4dot.netframework.sln
 msbuild -v:m -restore -t:Build -p:Configuration=Release -p:TargetFramework=net45 de4dot.netframework.sln
 if ($LASTEXITCODE) { exit $LASTEXITCODE }
 Remove-Item Release\net45\*.pdb, Release\net45\*.xml, Release\net45\Test.Rename.*
